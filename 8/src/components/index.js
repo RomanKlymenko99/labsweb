22ad84d8385f3666e7e9fcd42366969e17b1e434
RomanKlymenko99e <romanklymenko99@gmail.comm>
2024-01-14 20:07:03 +0200

    asdasd

---
+++ 8/src/components/index.js
@@ -0,0 +1,13 @@
+import './style.css';
+import todoinputComponent from './todo-input';
+import todolistComponent from './todo-list';
+
+function App () {
+  return (
+    <div className="App">
+      <todolistComponent/>
+    </div>
+  );
+}
+
+export default App;
